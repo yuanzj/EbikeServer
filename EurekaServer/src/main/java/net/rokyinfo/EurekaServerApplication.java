1decfe789a45bb1a84b19f678720728ef01f5e35
袁志健 <zhijian.yuan@rokyinfo.com>
2017-05-04 18:36:23 +0800

    init

---
+++ EurekaServer/src/main/java/net/rokyinfo/EurekaServerApplication.java
@@ -0,0 +1,15 @@
+package net.rokyinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(EurekaServerApplication.class).web(true).run(args);
+	}
+}
